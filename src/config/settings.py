8d33994f6c58eae616c2d32cdb83914ec6e54c29
eswarashish <ashish.kusumanchi@gmail.com>
2026-03-16 12:28:54 +0530

    chore: wire up with pydantic settings which includes hf model and dataset support

---
+++ src/config/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+from dotenv import load_dotenv
+load_dotenv()
+class Settings(BaseSettings):
+    model_config  = SettingsConfigDict(
+        extra="ignore",
+        case_sensitive=False
+    )
+    hf_dataset_path: str = Field(default="argilla/news-summary")
+    train_spilt: str = Field(default="train")
+    test_split: str = Field(default="test")
+    hf_model_name: str = Field(default="none")
