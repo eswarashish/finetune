a198dab1904baa3f9c768f58bbbdb20c5995bf82
eswarashish <ashish.kusumanchi@gmail.com>
2026-03-16 16:49:40 +0530

    feat: setup sft trainer args

--- src/scripts/sft.py
+++ src/scripts/sft.py
@@ -1,12 +1,17 @@
 from src.config.models.peft.lora import lora_config
 from src.config.models.sft import sft_config
-
+from src.data.argilla import test_dataset, train_dataset
 from trl.trainer.sft_trainer import SFTTrainer
 from src.config.settings import settings
 
 import mlflow
 import torch
-trainer = SFTTrainer(
-    model=settings.hf_model_name,
 
-)
+with mlflow.start_run(run_name=f'{settings.hf_model_name}'):
+    trainer = SFTTrainer(
+        model=settings.hf_model_name,
+        args=sft_config,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        peft_config=lora_config,
+    )
